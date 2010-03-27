5137a6c0d09c73d2b721cd326bf3a978a0c4a123
Kevin R. Barnes <vinbarnes@gmail.com>
2010-03-27 13:01:59 -0500

    Add db and base properties

--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -1,3 +1,15 @@
-describe Story do
+require File.expand_path(File.dirname(__FILE__) + '/../lib/story')
 
+describe Story do
+  it 'should have a description' do
+    Story.new.respond_to?(:description).should == true
+  end
+
+  it 'should have points' do
+    Story.new.respond_to?(:points).should == true
+  end
+
+  it 'should have a priority' do
+    Story.new.respond_to?(:priority).should == true
+  end
 end
