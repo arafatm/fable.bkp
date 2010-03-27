9c6fc44fbf169d278219750f4415c75effd1592c
Kevin R. Barnes <vinbarnes@gmail.com>
2010-03-27 13:01:59 -0500

    Add db and base properties

--- lib/story.rb
+++ lib/story.rb
@@ -1,3 +1,14 @@
+require 'dm-core'
+
+DataMapper.setup(:default, 'sqlite3:fable.db')
+
 class Story
+  include DataMapper::Resource
   
+  property :id, Serial
+  property :description, Text
+  property :points, Integer
+  property :priority, Integer
 end
+
+DataMapper.auto_upgrade!
