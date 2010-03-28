e6001134ab99a27aea044156c235e530af87b41e
Arafat Mohamed <github@arafatm.com>
2010-03-28 00:32:49 -0400

    Story default scope orders by priority

--- lib/story.rb
+++ lib/story.rb
@@ -3,12 +3,15 @@ require 'dm-core'
 DataMapper.setup(:default, 'sqlite3:fable.db')
 
 class Story
+
   include DataMapper::Resource
   
   property :id, Serial
   property :description, Text
   property :points, Integer
   property :priority, Integer
+
+  default_scope(:default).update(:order => [:priority.asc])
 end
 
 DataMapper.auto_upgrade!
--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -1,15 +1,36 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/story')
 
 describe Story do
-  it 'should have a description' do
-    Story.new.respond_to?(:description).should == true
+  describe 'attributes' do
+    it 'should have a description' do
+      Story.new.respond_to?(:description).should == true
+    end
+
+    it 'should have points' do
+      Story.new.respond_to?(:points).should == true
+    end
+
+    it 'should have a priority' do
+      Story.new.respond_to?(:priority).should == true
+    end
   end
 
-  it 'should have points' do
-    Story.new.respond_to?(:points).should == true
-  end
+  describe 'behaviour' do
+    before do
+      DataMapper.setup(:test, "sqlite3://test.db")
+      stories = Story.all
+      stories.destroy!
+    end
 
-  it 'should have a priority' do
-    Story.new.respond_to?(:priority).should == true
-  end
+    it 'should default order by priority' do
+      Story.new(:description => 'story 01', :points => 5, :priority => '4').save
+      Story.new(:description => 'story 02', :points => 5, :priority => '2').save
+      Story.new(:description => 'story 03', :points => 5, :priority => '6').save
+      stories = Story.all
+      puts stories.count
+      stories[0].priority.should.equal 2 
+      stories[1].priority.should.equal 4 
+      stories[2].priority.should.equal 6 
+    end
+  end 
 end
