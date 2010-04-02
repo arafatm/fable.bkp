3f1a7b34f61dee1c01bcc2c877a30ae6bf8064db
Arafat Mohamed <github@arafatm.com>
2010-04-01 20:05:10 -0400

    Added desrcribe attributes block in Status spec

--- spec/status_spec.rb
+++ spec/status_spec.rb
@@ -1,8 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/status')
 
 describe Status do
-
-  it 'should have a name' do
-    Status.new.respond_to?(:name).should == true
+  describe 'behaviour' do
+    it 'should have a name' do
+      Status.new.respond_to?(:name).should == true
+    end
   end
+
 end
