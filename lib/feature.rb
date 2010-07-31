4b55227983b48f586b74063984ad488b43c89099
Arafat Mohamed <github@arafatm.com>
2010-07-31 12:59:24 -0500

    get /feature/:id/stories

--- lib/feature.rb
+++ lib/feature.rb
@@ -17,3 +17,8 @@ get '/feature/:id' do
   content_type :json
   Feature.all(:id => params["id"]).to_json
 end
+
+get '/feature/:id/stories' do
+  content_type :json
+  Feature.all(:id => params["id"]).stories.to_json
+end
